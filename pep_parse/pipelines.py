47f3b3d3b83ac91a4c530d57c5a123f1d16b2992
Владислав Береснев <vladislaw.beresnev2018@yandex.ru>
2023-02-10 14:22:47 +0700

    Второй коммит 19 спринта

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,8 +1,10 @@
 import csv
+from csv import unix_dialect
 import datetime
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
+STR_RESULTS = 'results'
 DATE = datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
 
 
@@ -16,12 +18,16 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        results_dir = BASE_DIR / 'results'
+        results_dir = BASE_DIR / STR_RESULTS
         results_dir.mkdir(exist_ok=True)
         file_name = f'status_summary_{DATE}.csv'
         file_path = results_dir / file_name
         with open(file_path, mode='w', encoding='utf-8') as f:
             f.write('Статус,Количество\n')
-            writer = csv.writer(f, dialect='unix')
+            writer = csv.writer(
+                f,
+                dialect=unix_dialect,
+                quotechar='*',
+                quoting=csv.QUOTE_NONNUMERIC)
             writer.writerows(self.status.items())
-            f.write(f'Total,{sum(self.status.values())}\n')
+            f.write(f'-Total-,{sum(self.status.values())}\n')
